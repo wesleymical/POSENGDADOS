1660daef99c93d3e2e83c61f9b3837122e03ec44
Wesley Mical <45857369+wesleymical@users.noreply.github.com>
2024-10-27 18:06:28 -0300

    Master

--- ESTRUTURA_DADOS/ATIVIDADE/06.py
+++ ESTRUTURA_DADOS/ATIVIDADE/06.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import datetime
+import zipfile
 
 #=== PARAMETROS ========================================================================================
 # Lista de UFs do Brasil
@@ -23,135 +24,220 @@ def gravar_log(mensagem, diretorio_log):
     with open(os.path.join(diretorio_log, 'log-06.txt'), 'a') as log_file:
         log_file.write(mensagem + '\n')
 
+# arquivos de download
+arquivos = dict_diretorio['.zip']
+
+#repositorio de extracao
+repositorio = dict_diretorio['.csv']
+
 #=== MAIN ==============================================================================================
 # Função principal
 def main():
-    download = 'D:/mical/Documents/POS ENGENHARIA DE DADOS/MATERIAS/ED/Download'
     mensagem = "Principal - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
     print(mensagem)
-    gravar_log(mensagem, download)
-    
-    mover_arquivos(download)
-    mover_restantes_para_outros(download)
-    excluir_pastas_vazias(download)
-    
+    gravar_log(mensagem, arquivos)
+
+    # 1 - Limpar repositorio
+    limpar_diretorio(repositorio, arquivos)
+    # 2 - Descompactar os arquivos
+    descompactar_arquivos(repositorio, arquivos)
+    # 3 - Mover arquivos CSV
+    mover_csv(repositorio, arquivos)
+    # 4 - Mover arquivos PDF
+    mover_pdf(repositorio, arquivos)
+    # 5 - Excluir pastas vazias
+    excluir_pastas_vazias(repositorio, arquivos)
+
     mensagem = "Principal - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
     print(mensagem)
-    gravar_log(mensagem, download)
+    gravar_log(mensagem, arquivos)
 
 #=== SUB FUNCOES =======================================================================================
-# Função para mover e renomear arquivos com base na extensão da dict
-def mover_arquivos(diretorio_origem):
+# Função para mover arquivos csv
+def mover_csv(repositorio, arquivos):
     try:
-        mensagem = "Função: mover_arquivos - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+        mensagem = "Função: mover_csv - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
         
-        for root, dirs, files in os.walk(diretorio_origem):
+        for root, dirs, files in os.walk(repositorio):
+
             for file in files:
                 extensao = os.path.splitext(file)[1]
+
                 if extensao in dict_diretorio:
                     caminho_atual = os.path.join(root, file)
-                    uf = file.split('_')[-1].replace(extensao, '')  # Extrai a UF do nome do arquivo
+
+                    if extensao == '.csv':
+                        uf = file.split('_')[-1].replace(extensao, '')  # Extrai a UF do nome do arquivo
                     
-                    # Valida se a UF está na lista de UFs
-                    if uf in list_uf:
-                        # Cria o diretório da UF se não existir
-                        diretorio_uf = os.path.join(dict_diretorio[extensao], uf)
-                        if not os.path.exists(diretorio_uf):
-                            os.makedirs(diretorio_uf)
-                            mensagem = f'Diretório {diretorio_uf} criado'
-                            print(mensagem)
-                            gravar_log(mensagem, diretorio_origem)
-                    else:
-                        # Atualiza a lista de UFs e o dicionário de diretórios para incluir "Outro"
-                        if 'Outros' not in list_uf:
-                            list_uf.append('Outros')
-                        if 'Outros' not in dict_diretorio:
-                            dict_diretorio['Outros'] = 'D:/mical/Documents/POS ENGENHARIA DE DADOS/MATERIAS/ED/ATIVIDADE/OUTROS'
+                        # Valida se a UF está na lista de UFs
+                        if uf in list_uf:
+                            # Cria o diretório da UF se não existir
+                            diretorio_uf = os.path.join(dict_diretorio[extensao], uf)
+
+                            if not os.path.exists(diretorio_uf):
+                                os.makedirs(diretorio_uf)
+                                mensagem = f'Diretório {diretorio_uf} criado'
+                                print(mensagem)
+                                gravar_log(mensagem, arquivos)
+                        else:
+                            # Atualiza a lista de UFs e o dicionário de diretórios para incluir "Outros"
+                            if 'Outros' not in list_uf:
+                                list_uf.append('Outros')
+                            if 'Outros' not in dict_diretorio:
+                                dict_diretorio['Outros'] = 'D:/mical/Documents/POS ENGENHARIA DE DADOS/MATERIAS/ED/ATIVIDADE/OUTROS'
+
+                            diretorio_uf = os.path.join(dict_diretorio['Outros'], uf)
+
+                            if not os.path.exists(diretorio_uf):
+                                os.makedirs(diretorio_uf)
+                                mensagem = f'Diretório {diretorio_uf} criado para arquivos não classificados'
+                                print(mensagem)
+                                gravar_log(mensagem, arquivos)
+
+                        caminho_destino = os.path.join(diretorio_uf, file)
                         
-                        diretorio_uf = os.path.join(dict_diretorio['Outros'], uf)
-                        if not os.path.exists(diretorio_uf):
-                            os.makedirs(diretorio_uf)
-                            mensagem = f'Diretório {diretorio_uf} criado para arquivos não classificados'
-                            print(mensagem)
-                            gravar_log(mensagem, diretorio_origem)
-                        
-                    caminho_destino = os.path.join(diretorio_uf, file)
-                    
+                        # Move arquivos
+                        shutil.move(caminho_atual, caminho_destino)
+                        mensagem = f'Movido {caminho_atual} para {caminho_destino}'
+                        print(mensagem)
+                        gravar_log(mensagem, arquivos)
+        
+        mensagem = "Função: mover_csv - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
+
+    except Exception as e:
+        mensagem = 'Erro ao processar mover_csv: ' + str(e)
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
+        raise e
+        
+# Funcão para mover arquivos pdf
+def mover_pdf(repositorio, arquivos):
+    try:
+        mensagem = "Função: mover_pdf - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
+
+        for root, dirs, files in os.walk(repositorio):
+
+            # Remover "Outros" de dirs para que os subdiretórios não sejam percorridos
+            dirs[:] = [d for d in dirs if d != 'OUTROS']
+
+            # Garantir que a pasta "Outros" seja ignorada
+            if 'OUTROS' in root:
+                continue
+
+            for file in files:
+                extensao = os.path.splitext(file)[1]
+                
+                if extensao in dict_diretorio:
+                    caminho_atual = os.path.join(root, file)
+
                     # Renomeia e move PDFs
                     if extensao == '.pdf':
                         nome_pasta = os.path.basename(os.path.dirname(caminho_atual))
                         novo_nome_pdf = f'{os.path.splitext(file)[0]}_{nome_pasta}.pdf'
-                        caminho_novo_pdf = os.path.join(diretorio_uf, novo_nome_pdf)
+                        caminho_novo_pdf = os.path.join(dict_diretorio['Outros'], novo_nome_pdf)
                         os.rename(caminho_atual, caminho_novo_pdf)
                         mensagem = f'Renomeado e movido {caminho_atual} para {caminho_novo_pdf}'
                         print(mensagem)
-                        gravar_log(mensagem, diretorio_origem)
-                    else:
-                        # Move demais arquivos
-                        shutil.move(caminho_atual, caminho_destino)
-                        mensagem = f'Movido {caminho_atual} para {caminho_destino}'
-                        print(mensagem)
-                        gravar_log(mensagem, diretorio_origem)
-        
-        mensagem = "Função: mover_arquivos - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+                        gravar_log(mensagem, arquivos)
+
+        mensagem = "Função: mover_pdf - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
+
     except Exception as e:
-        mensagem = 'Erro ao processar mover_arquivos: ' + str(e)
+        mensagem = 'Erro ao processar mover_pdf: ' + str(e)
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
+        raise e
+
+# Função para descompactar arquivos
+def descompactar_arquivos(repositorio, arquivos):
+    try:
+        mensagem = "Função: descompactar_arquivos - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
+
+        for root, dirs, files in os.walk(arquivos):
+
+            for file in files:
+
+                if file.endswith('.zip'):
+                    zip = os.path.join(root, file)
+                    extracao = os.path.splitext(file)[0]
+                    extraidos = os.path.join(repositorio, extracao)
+
+                    with zipfile.ZipFile(zip, 'r') as zip_ref:
+                        zip_ref.extractall(extraidos)
+                        print(f'Descompactado {zip} para {extraidos}')
+                        gravar_log(f'Descompactado {zip} para {extraidos}', arquivos)
+
+        mensagem = "Função: descompactar_arquivos - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
+
+    except Exception as e:
+        mensagem = 'Erro ao descompactar_arquivos: ' + str(e)
+        print(mensagem)
+        gravar_log(mensagem, arquivos)
         raise e
 
 # Função para excluir pastas vazias
-def excluir_pastas_vazias(diretorio_origem):
+def excluir_pastas_vazias(repositorio, arquivos):
     try:
         mensagem = "Função: excluir_pastas_vazias - Inicio - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
         
-        for root, dirs, files in os.walk(diretorio_origem, topdown=False):
+        for root, dirs, files in os.walk(repositorio, topdown=False):
+
             for dir in dirs:
                 caminho_diretorio = os.path.join(root, dir)
+                print(f'Verificando diretório: {caminho_diretorio}')
+                gravar_log(caminho_diretorio, arquivos)
+
                 if not os.listdir(caminho_diretorio):  # Verifica se a pasta está vazia
                     os.rmdir(caminho_diretorio)
                     mensagem = f'Pasta vazia {caminho_diretorio} excluída'
                     print(mensagem)
-                    gravar_log(mensagem, diretorio_origem)
+                    gravar_log(mensagem, arquivos)
         
         mensagem = "Função: excluir_pastas_vazias - Fim - " + str(datetime.datetime.now().strftime("%H:%M:%S"))
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
+
     except Exception as e:
         mensagem = 'Erro ao processar excluir_pastas_vazias: ' + str(e)
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
         raise e
 
-# Função para mover arquivos restantes para o diretório "Outro"
-def mover_restantes_para_outros(diretorio_origem):
+def limpar_diretorio(repositorio, arquivos):
     try:
-        outro_diretorio = dict_diretorio['Outros']
-        if not os.path.exists(outro_diretorio):
-            os.makedirs(outro_diretorio)
-            mensagem = f'Diretório {outro_diretorio} criado'
-            print(mensagem)
-            gravar_log(mensagem, diretorio_origem)
-        
-        for root, dirs, files in os.walk(diretorio_origem):
+        for root, dirs, files in os.walk(repositorio, topdown=False):
             for file in files:
-                caminho_atual = os.path.join(root, file)
-                caminho_destino = os.path.join(outro_diretorio, file)
-                shutil.move(caminho_atual, caminho_destino)
-                mensagem = f'Movido {caminho_atual} para {caminho_destino}'
-                print(mensagem)
-                gravar_log(mensagem, diretorio_origem)
+                caminho_arquivo = os.path.join(root, file)
+                os.remove(caminho_arquivo)
+                print(f'Arquivo {caminho_arquivo} excluído')
+                
+            for dir in dirs:
+                caminho_diretorio = os.path.join(root, dir)
+                os.rmdir(caminho_diretorio)
+                print(f'Diretório {caminho_diretorio} excluído')
+        
+        print(f'Diretório {repositorio} limpo')
+
     except Exception as e:
-        mensagem = 'Erro ao processar mover_restantes_para_outros: ' + str(e)
+        mensagem = 'Erro ao limpar_diretorio: ' + str(e)
         print(mensagem)
-        gravar_log(mensagem, diretorio_origem)
+        gravar_log(mensagem, arquivos)
         raise e
+
 
 #=== END ===============================================================================================
 # Execução do main
